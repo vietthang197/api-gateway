01ab4be076fa2d8aeeb487a36611812698721fb9
acer <thanglv@neo.vn>
2022-03-16 17:57:31 +0700

    up

--- src/main/java/vn/neo/App.java
+++ src/main/java/vn/neo/App.java
@@ -3,11 +3,9 @@ package vn.neo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableWebFluxSecurity
 public class App {
 
     public static void main(String[] args) {
